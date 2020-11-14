d82b1b5a2c65e570fa58044054862e1575fb0d2e
JamesApple <james@jamesapple.com>
2020-11-14 22:12:05 +1100

    Add result test

---
+++ src/Result.test.ts
@@ -0,0 +1,16 @@
+import {Result} from "./Result";
+
+describe("Result", () => {
+  const aResult = "a-result";
+  it("Should unwrap a promise", async function () {
+    const promise = Promise.resolve(aResult);
+    const result = await Result.fromPromise<string, Error>(promise);
+    expect(result.getEither()).toEqual(aResult);
+  });
+
+  it("Should catch a rejected promise", async function () {
+    const promise = Promise.reject(aResult);
+    const result = await Result.fromPromise<string, Error>(promise);
+    expect(result.getEither()).toEqual(aResult);
+  });
+});
